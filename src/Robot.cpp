1c2ae4cee9c94c13a805a85fb75b724d70cd0eee
Sourec <tothemyers@gmail.com>
2016-01-28 16:51:08 -0500

    Add NavX MXP library files, modified for compilation without config

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,42 +1,20 @@
 #include "WPILib.h"
 #include "../ADBLib/src/ADBLib.h"
-using ADBLib::Logger;
-using ADBLib::Drivebase;
-using ADBLib::TractionDrive;
+#include "../lib/navx_frc_cpp/include/AHRS.h"
 
 class Robot: public IterativeRobot
 {
 private:
-	Joystick* jys;
-	Compressor* compressor;
-	BuiltInAccelerometer* bia;
-	Drivebase* drivebase;
-	CANTalon* motors[4];
+
 
 	void RobotInit()
 	{
-		Logger::newLog("sysLog", "/sysLog.txt");
-		compressor = new Compressor(0);
-		jys = new Joystick(0);
-		bia = new BuiltInAccelerometer;
-		for (int i = 0; i < 4; i++)
-		{
-			motors[i] = new CANTalon(i + 1);
-			motors[i]->SetControlMode(CANTalon::kPercentVbus);
-		}
 
-		drivebase = new TractionDrive(motors[0], motors[1], motors[2], motors[3]);
-		motors[3]->SetInverted(true);
-		motors[1]->SetInverted(true);
-
-
-
-		Logger::log("Successfully initialized Robot", "sysLog");
 	}
 
 	void AutonomousInit()
 	{
-		compressor->Start();
+
 	}
 
 	void AutonomousPeriodic()
@@ -46,31 +24,22 @@ private:
 
 	void TestInit()
 	{
-		compressor->Start();
+
 	}
 
 	void TeleopInit()
 	{
-		compressor->Start();
+
 	}
 
 	void TeleopPeriodic()
 	{
-		double aX = 0, aY = 0, aZ = 0;
-		aX = bia->GetX();
-		aY = bia->GetY();
-		aZ = bia->GetZ();
-		SmartDashboard::PutNumber("aX", aX);
-		SmartDashboard::PutNumber("aY", aY);
-		SmartDashboard::PutNumber("aZ", aZ);
-
-		drivebase->drive(jys->GetRawAxis(2), jys->GetRawAxis(1), jys->GetRawAxis(0));
 
 	}
 
 	void DisabledInit()
 	{
-		compressor->Stop();
+
 	}
 };
 
