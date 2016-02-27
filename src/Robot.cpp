d3d90fdd02a65f68ffed9155c2779866da029071
Ethan and Isaac <isaacgridley@gmail.com>
2016-02-27 13:16:54 -0500

    Deadreckoning position 3

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -2,6 +2,8 @@
 #include "../ADBLib/src/ADBLib.h"
 using namespace ADBLib;
 
+void rotate(double degrees, Drivebase* drivebase, AHRS* ahrs);
+
 class Robot: public IterativeRobot
 {
 private:
@@ -12,12 +14,16 @@ private:
 	Compressor* compressor;
 	Preferences* prefs;
 
+	AHRS* ahrs;
 	SimplePneumatic* shooterPiston;
 	SimplePneumatic* liftArm;
 	SimplePneumatic* extendArm;
 	SimplePneumatic* tail;
 	SimplePneumatic* mandibles;
 	CANTalon* fan;
+
+	Timer timer;
+	double first_maneuver_time = 2.5;
 
 	void RobotInit()
 	{
@@ -36,6 +42,7 @@ private:
 		motors[1]->SetInverted(true);
 		drivebase = new TractionDrive(motors[4], motors[2], motors[3], motors[1]);
 
+		ahrs = new AHRS(SPI::Port::kMXP);
 		liftArm = new SimplePneumatic(new DoubleSolenoid(0, 1));
 		shooterPiston = new SimplePneumatic(new Solenoid(3));
 		extendArm = new SimplePneumatic(new Solenoid(2));
@@ -48,15 +55,43 @@ private:
 	void AutonomousInit()
 	{
 		compressor->Start();
+		ahrs->ZeroYaw();
 	}
 
 	void AutonomousPeriodic()
 	{
-		if (prefs->GetBoolean("fan-on", true))
-			fan->Set(1);
-		else
-			fan->Set(0);
-		//drivebase->drive(0, -0.6, -(ahrs->GetYaw() / 18.0)); //y and x axis flipped?
+		//if (prefs->GetBoolean("fan-on", true))
+			//fan->Set(1);
+		//else
+		//fan->Set(0);
+			//Using Gyroscope and dead reckoning to move from #3 to front of tower
+		timer.Start();
+		while(timer.Get() < first_maneuver_time)
+		{
+			drivebase->drive(0, 0.6, -(ahrs->GetYaw() / 18.0));
+		}
+		timer.Stop();
+		drivebase->drive(0, 0, 0);
+		Wait(0.2);
+
+		rotate(45, drivebase, ahrs);
+		drivebase->drive(0, 0.5, 0);
+		Wait(1.0);
+		rotate(-45.0, drivebase, ahrs);
+		drivebase->drive(0, 0.5, 0);
+		Wait(0.7);
+		drivebase->drive(0, 0, 0);
+		Wait(1000000000);
+		/*while(fabs(ahrs->GetYaw() - fdest) > 5);
+		{
+			double diff = ahrs->GetYaw() - fdest;
+			if (diff > 180.0)
+				diff = 360.0 - diff;
+			if (diff < -180)
+				diff = 360 + diff;
+
+			drivebase->drive(0, 0.6, -(diff / 90.0));
+		}*/
 	}
 
 	void TeleopInit()
@@ -95,5 +130,27 @@ private:
 
 	}
 };
+
+void rotate(double degrees, Drivebase* drivebase, AHRS* ahrs)
+{
+	SmartDashboard::PutNumber("yaw", ahrs->GetYaw());
+	double startRot = ahrs->GetYaw();
+	double fdest = degrees + startRot;
+	if (fdest > 360.0)
+		fdest -= 360.0;
+
+	while(fabs(ahrs->GetYaw() - fdest) > 5)
+	{
+		double diff = ahrs->GetYaw() - fdest;
+		if (diff > 180.0)
+			diff = 360.0 - diff;
+		if (diff < -180)
+			diff = 360 + diff;
+
+		SmartDashboard::PutNumber("diff", diff);
+		drivebase->drive(0, 0, -diff / 45.0);
+	}
+	drivebase->drive(0, 0, 0);
+}
 
 START_ROBOT_CLASS(Robot);
