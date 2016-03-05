1d4f72ddc423931465cef439e04c18c783fbeed5
Sourec <tothemyers@gmail.com>
2016-03-05 07:48:48 -0500

    Add auto elevation to GuidedDrive state + backup "no guidance" condition
    Auto elevation will attempt to keep the "best" vision target's center (y) between
    the 3/4 and 1/4 mark on the vertical axis of the camera image. If there are no
    vision targets at all, the state will begin unguided driving by maintaining the
    default "diff" value of zero.

--- src/auton/GuidedDrive.cpp
+++ src/auton/GuidedDrive.cpp
@@ -22,17 +22,28 @@ int GuidedDrive::update()
 	//Returns a shared_ptr object.
 	auto grip = NetworkTable::GetTable("grip"); //Returns an std::vector! DO NOT USE sizeof(container)/sizeof(element)!
 	auto areas = grip->GetNumberArray("targetsCam0/area", llvm::ArrayRef<double>());
+
+	//If the target goes out of view of the camera, the robot will continue driving, just with no correction.
 	double diff = 0;
 	if (areas.size() != 0)
 	{
+		//Select the best target (defined by target with largest area) by index. std::vector<>::iterators won't work here.
 		unsigned int bestTarget = 0;
-
 		for (unsigned int i = 0; i < areas.size(); i++)
 			if (areas[i] > areas[bestTarget])
 				bestTarget = i;
 
 		diff = (CAM_XSIZE / 2.0) - grip->GetNumberArray("targetsCam0/centerX", llvm::ArrayRef<double>())[bestTarget];
 		diff *= CAM_ANGRESO;
+
+		//Use vision to keep the center of the target between the 1/4 and 3/4 marks
+		double bestY = grip->GetNumberArray("targetsCam0/centerY", llvm::ArrayRef<double>())[bestTarget];
+		if (bestY > (3 * CAM_YSIZE / 4.0))
+			armUp();
+		else if (bestY < CAM_YSIZE / 4.0)
+			armDown();
+		else
+			armStop();
 	}
 	drive(speed, diff * ROTATE_SCALAR);
 
