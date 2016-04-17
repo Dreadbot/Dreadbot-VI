927aece5da7fa5766dd505949151ec1ad0476b20
Sourec <tothemyers@gmail.com>
2016-04-17 19:59:04 -0400

    More auton timing/rotation tweaks

--- src/auton/RoboState.h
+++ src/auton/RoboState.h
@@ -12,7 +12,7 @@ using std::stack;
 #define DRIVE_KD 0.003
 
 //NavX MXP collision constants
-#define COLLISION_THRESHOLD_DELTAG 	0.6 	//Acceleration must exceed this limit for a collision to be registered
+#define COLLISION_THRESHOLD_DELTAG 	0.7 	//Acceleration must exceed this limit for a collision to be registered
 #define COLLISION_TIMEOUT 			0.06 	//60ms timeout to ensure valid collision detection. May be too generous.
 #define COLLISION_DELAY				0.5		//Time to wait until collisions are registered. Used in drive/guideddrive states
 
@@ -23,16 +23,17 @@ using std::stack;
 #define CAM_ANGRESO (CAM_FOV / CAM_XSIZE)	//Calculated constant
 
 //Autonomous constants
-#define BREACH_TIME			2.35			//Time taken to breach terrain defenses
-#define LOWBAR_TIME			1.70			//Time taken to breach the lowbar.
+#define BREACH_TIME			2.55			//Time taken to breach terrain defenses
+#define LOWBAR_TIME			2.20			//Time taken to breach the lowbar.
 #define BREACH_SPEED		0.75			//Speed (-1-1) of driving while breaching.
 #define LOWBAR_ANGLE		65.3			//Angle to rotate by after passing through the low bar
 #define SHOOT_TIME			0.3				//How long the shooter piston should be extending/extended in order to shoot
 #define ARMUP_PULSETIME 	0.25 			//Time to pulse the arm to raise cameras above the bumpers.
-#define ROTATE_SCALAR		(1.0 / 90.0)	//Value to multiply error by to rotate at the right speed
+#define ROTATE_SCALAR		(1.0 / 85.0 )	//Value to multiply error by to rotate at the right speed
 #define DRIVE_SCALAR		(1.0 / 18.0)	//Value to multiply error by to drive straight
 #define ARMUP_TIME			0.5				//Time to raise the arm from lowest to highest position
 #define ARMOUT_SHOOTERDELAY	0.49			//Time delay until the shooter piston should eject the ball after the arm starts extending
+#define ROT_STDSTATE_COR	0.075			//Basine rotation rate, applied to fix steady state-ish error.
 
 class RoboState : public FSMState
 {
--- src/auton/Rotate.cpp
+++ src/auton/Rotate.cpp
@@ -18,7 +18,7 @@ void Rotate::enter()
 	autoLog->log("Entering state 'Rotate' and rotating from " + std::to_string(getYaw()) + " to " + std::to_string(getYaw() + dest) + " degrees...");
 	startRot = getYaw();
 	armUp();
-	Wait(0.40); //eh....
+	Wait(0.30); //eh....
 	armStop();
 }
 
@@ -35,7 +35,7 @@ int Rotate::update()
 	if (diff < -180.0)
 		diff = 360 + diff;
 
-	drive(0, -diff * ROTATE_SCALAR);
+	drive(0, (-diff * ROTATE_SCALAR) + std::copysign(-diff, ROT_STDSTATE_COR));
 
 	if (fabs(diff) <= 3.0)
 		return TIMER_EXPIRED;
@@ -45,4 +45,6 @@ int Rotate::update()
 void Rotate::exit()
 {
 	autoLog->log("Leaving state 'Rotate' with rotation of " + std::to_string(getYaw()) + " degrees...");
+	drive(0, 0);
+	Wait(0.3);
 }
